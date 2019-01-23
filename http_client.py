d998a096bbde0bfe9733ce3959b04d0558a7e883
alexisbdr <alexis.baudron15@gmail.com>
2019-01-23 17:22:57 -0600

    connect to socket and parse url

--- http_client.py
+++ http_client.py
@@ -1,8 +1,28 @@
 import socket
+import sys
+from urlparse import urlparse
 
 def main(): 
-    raise NotImplementedError 
+
+	url = str(sys.argv[1])
+
+	parsed_url = urlparse(url)
+	port = parsed_url.port if parsed_url.port else 80
+	host = parsed_url.hostname if parsed_url.hostname else url
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	s.connect((url, port))
+
+	request_message = "GET / HTTP/1.1\nHost:" + host + "\n\n"
+	s.send(request_message)
+
+	result = s.recv(4096)
+
+	print(result)
+
+
 
 if __name__ == "__main__": 
-    main() 
+	main() 
 
