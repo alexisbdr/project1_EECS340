812fd0e49dfda6713990b05442cc2c01fc8b6c36
alexisbdr <alexis.baudron15@gmail.com>
2019-01-25 21:16:22 -0600

    part1 done

--- http_client.py
+++ http_client.py
@@ -2,6 +2,8 @@ import socket
 import sys
 from urlparse import urlparse
 
+CRLF = '\r\n\r\n'
+
 def parse_url(url):
 
 	parsed_url = urlparse(url)
@@ -26,25 +28,100 @@ def parse_url(url):
 
 	return port, url, request_message
 
+def receive_page_chunks(sock):
+	
+	CHUNK_SIZE = 4096
 
-def main(): 
+	chunks = []
+	while True: 
+		chunk = sock.recv(CHUNK_SIZE)
+		if chunk: 
+			chunks.append(chunk)
+		else: 
+			break
 
-	url = str(sys.argv[1])
+	return ''.join(chunks)
+
+def get_response_code(data):
+
+	http_header = data.split(CRLF,1)[0]
+	request = http_header.split('\n')[0]
+	response_code = request.split()[1]
+
+	return int(response_code)
+
+def get_response_content_type(data):
+
+	http_header = data.split(CRLF,1)[0].split('\n')
+	for line in http_header: 
+		if len(line) > 12 and line[0:6] == 'Content':
+			break
+	content_type = line.split()[1].strip(';')
+
+	return content_type == 'text/html'
+
+def get_response_body(data):
+
+	http_body = data.split(CRLF,1)[1]
+	
+	return http_body
+ 
+def get_response_redirect_location(data):
+
+	http_header = data.split(CRLF,1)[0].split('\n')
+	for line in http_header:
+		if line[0:8] == 'Location':
+			break
+	redirect_url = line.split()[1]
+
+	return redirect_url 
+
+def GET_request(url):
 
 	port, url, request_message = parse_url(url)
-	print(port, url, request_message)
 	
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+	
 	s.connect((url, port))
 	
 	s.send(request_message)
+	
+	chunks = receive_page_chunks(s)
 
-	result = s.recv(4096)
+	response_code = get_response_code(chunks)
 
-	print(result)
+	return response_code, chunks
 
+def main(): 
 
+	if len(sys.argv) < 2: 
+		sys.stderr.write("ERROR no URL provided")
+		sys.exit(1)
+	elif len(sys.argv) > 2: 
+		sys.stderr.write("ERROR more than one URL provided")
+		sys.exit(1)
+
+	url = str(sys.argv[1])
+	num_redirect = 0
+	while num_redirect < 10: 
+
+		response_code, chunks = GET_request(url)
+
+		if response_code == 200: 
+			if get_response_content_type(chunks): 
+				sys.stdout.write(get_response_body(chunks))
+			sys.exit(0)
+
+		if response_code == 301 or response_code == 302: 
+			url = get_response_redirect_location(chunks)
+			redirect_message = 'Redirected to: ' + str(url) + '\n'
+			sys.stderr.write(redirect_message)
+			num_redirect+=1
+			continue
+
+		if response_code >= 400: 
+			sys.stdout.write(get_response_body(chunks))
+			sys.exit(1)
 
 if __name__ == "__main__": 
 	main() 
