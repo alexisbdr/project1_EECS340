5ed045333c5c5a4ff5de32560febdc8966aac092
Ignacio de Osma <Ignacio@dhcp-10-105-57-95.wireless.northwestern.private>
2019-01-27 17:35:16 -0600

    error message for 10 redirects

--- http_client.py
+++ http_client.py
@@ -16,11 +16,11 @@ def parse_url(url):
 
 	url = host
 
-	if scheme == 'https': 
+	if scheme == 'https':
 		sys.stderr.write('ERROR page requires encryption \n')
 		sys.exit(1)
 
-	if scheme == '': 
+	if scheme == '':
 		sys.stderr.write('ERROR url does not start with http:// \n')
 		sys.exit(1)
 
@@ -29,15 +29,15 @@ def parse_url(url):
 	return port, url, request_message
 
 def receive_page_chunks(sock):
-	
+
 	CHUNK_SIZE = 1024
 
 	chunks = []
-	while True: 
+	while True:
 		chunk = sock.recv(CHUNK_SIZE)
-		if chunk: 
+		if chunk:
 			chunks.append(chunk)
-		else: 
+		else:
 			break
 
 	return ''.join(chunks)
@@ -53,7 +53,7 @@ def get_response_code(data):
 def get_response_content_type(data):
 
 	http_header = data.split(CRLF,1)[0].split('\n')
-	for line in http_header: 
+	for line in http_header:
 		if len(line) > 12 and line[0:6] == 'Content':
 			break
 	content_type = line.split()[1].strip(';')
@@ -63,9 +63,9 @@ def get_response_content_type(data):
 def get_response_body(data):
 
 	http_body = data.split(CRLF,1)[1]
-	
+
 	return http_body
- 
+
 def get_response_redirect_location(data):
 
 	http_header = data.split(CRLF,1)[0].split('\n')
@@ -74,55 +74,57 @@ def get_response_redirect_location(data):
 			break
 	redirect_url = line.split()[1]
 
-	return redirect_url 
+	return redirect_url
 
 def GET_request(url):
 
 	port, url, request_message = parse_url(url)
-	
+
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	
+
 	s.connect((url, port))
-	
+
 	s.send(request_message)
-	
+
 	chunks = receive_page_chunks(s)
 
 	response_code = get_response_code(chunks)
 
 	return response_code, chunks
 
-def main(): 
+def main():
 
-	if len(sys.argv) < 2: 
+	if len(sys.argv) < 2:
 		sys.stderr.write("ERROR no URL provided \n")
 		sys.exit(1)
-	elif len(sys.argv) > 2: 
+	elif len(sys.argv) > 2:
 		sys.stderr.write("ERROR more than one URL provided \n")
 		sys.exit(1)
 
 	url = str(sys.argv[1])
 	num_redirect = 0
-	while num_redirect < 10: 
+	while num_redirect < 10:
 
 		response_code, chunks = GET_request(url)
 
-		if response_code == 200: 
-			if get_response_content_type(chunks): 
+		if response_code == 200:
+			if get_response_content_type(chunks):
 				sys.stdout.write(get_response_body(chunks))
 			sys.exit(0)
 
-		if response_code == 301 or response_code == 302: 
+		if response_code == 301 or response_code == 302:
 			url = get_response_redirect_location(chunks)
 			redirect_message = 'Redirected to: ' + str(url) + '\n'
 			sys.stderr.write(redirect_message)
 			num_redirect+=1
 			continue
 
-		if response_code >= 400: 
+		if response_code >= 400:
 			sys.stdout.write(chunks)
 			sys.exit(1)
 
-if __name__ == "__main__": 
-	main() 
+	sys.stderr.write("ERROR more than 10 redirects \n")
+	sys.exit(1)
 
+if __name__ == "__main__":
+	main()
