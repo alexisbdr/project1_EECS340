699ad68ceebf4ccc128e61937bd9e60d670f0734
alexisbdr <alexis.baudron15@gmail.com>
2019-01-27 17:17:51 -0600

    took out main function

--- http_server1.py
+++ http_server1.py
@@ -35,39 +35,19 @@ class Socket:
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		
 		self.sock.bind((self.host, self.port))
-	
-		self
+
+		self.run_forever()
+
+		
 	def run_forever():
 
 		while True: 
 
-			sock.listen(1)
+			sock.listen(1) #1 means accept single connection
 
 			(client_socket, client_address) = sock.accept()
 
 			sock.recv(CHUNK)
-
-
-def main(): 
-
-	if len(sys.argv) < 2: 
-		sys.stderr.write("ERROR no port provided")
-		sys.exit(1)
-	elif len(sys.argv) > 2: 
-		sys.stderr.write("ERROR more than one porrt provided")
-		sys.exit(1)
-
-	port = int(sys.argv[1])
-
-	if port < 1024: 
-		sys.stderr.write('ERROR - port number must be above 1024')
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	s.bind((socket.gethostname(), port))
-
-	run_forever(s)
-
 
 
 if __name__ == "__main__":
