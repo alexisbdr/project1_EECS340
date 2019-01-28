da748724c0dd92ec651424e295db11b549e3aef4
Ignacio de Osma <Ignacio@dhcp-10-105-57-95.wireless.northwestern.private>
2019-01-28 17:36:40 -0600

    cleaning http_server 1 and 2

--- http_server1.py
+++ http_server1.py
@@ -1,6 +1,6 @@
 import socket
-import sys 
-from urlparse import urlparse 
+import sys
+from urlparse import urlparse
 import os
 import time
 import signal
@@ -16,41 +16,41 @@ def get_last_modified_time(path):
 	if platform.system() == 'Windows':
 
 		return os.path.getctime(path)
-	
+
 	else:
-	
+
 		stat = os.stat(path)
-	
+
 		try:
-	
+
 			return stat.st_birthtime
-	
+
 		except AttributeError:
-	
+
 			return stat.st_mtime
 
 
 def generate_http_header(code, path):
-	
+
 	responses = {
 	200: '200 OK',
 	403: '403 Forbidden',
 	404: '404 Not Found'}
 
 	http_header = 'HTTP/1.1 ' + responses[code] + '\n'
-	
+
 	current_date = 'Date: ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + ' GMT\n'
 	http_header += current_date
 
 	'''
 	Last modified stuff
-	if code == 200: 
-		last_modified_time = 'Last Modified: ' + get_last_modified_time(path) + ' GMT' 
+	if code == 200:
+		last_modified_time = 'Last Modified: ' + get_last_modified_time(path) + ' GMT'
 	'''
 	server_name = 'Server: http_server1 (Simple)\n'
 	http_header += server_name
 
-	if code == 200: 
+	if code == 200:
 		http_header += 'Content-Type: text/html; charset=UTF-8\n'
 
 	CLRF = '\r\n\r\n'
@@ -58,8 +58,8 @@ def generate_http_header(code, path):
 	http_header+=connection_close
 
 	print(http_header)
-	
-	return http_header 
+
+	return http_header
 
 
 def generate_http_response(code, path):
@@ -70,16 +70,16 @@ def generate_http_response(code, path):
 		try:
 			file = open(path, 'r')
 			response_body = file.read()
-		except: 
+		except:
 			#If failure, 404 Not Found
 			generate_http_response(404, path)
-	
-	elif code == 403: 
-		
+
+	elif code == 403:
+
 		response_body = "<html><body><p>Error 403: Forbidden</p>"
-	
+
 	elif code == 404:
-		
+
 		response_body = "<html><body><p>Error 404: Not Found</p>"
 
 	response = response_header + response_body
@@ -108,16 +108,16 @@ def get_file_name(data):
 
 def ends_with_(name):
 
-	if name[-4:] == ".htm" or name[-4:] == "html": 
+	if name[-4:] == ".htm" or name[-4:] == "html":
 		return False
 
 
 def check_command_input():
 
-	if len(sys.argv) < 2: 
-		sys.stderr.write("ERROR no port provided")
+	if len(sys.argv) < 2:
+		sys.stderr.write("ERROR no port provided \n")
 		sys.exit(1)
-	elif len(sys.argv) > 2: 
+	elif len(sys.argv) > 2:
 		sys.stderr.write("ERROR more than one porrt provided")
 		sys.exit(1)
 
@@ -126,7 +126,7 @@ class Socket:
 
 	CHUNK = 1024
 
-	def __init__(self): 
+	def __init__(self):
 
 		check_command_input()
 
@@ -140,7 +140,7 @@ class Socket:
 		self.create_socket()
 
 
-	def shutdown(self): 
+	def shutdown(self):
 
 		self.sock.shutdown(socket.SHUT_RDWR)
 
@@ -150,15 +150,15 @@ class Socket:
 	def create_socket(self):
 
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		
+
 		self.sock.bind((self.host, self.port))
 
 		self.run_forever()
 
-		
+
 	def run_forever(self):
 
-		while True: 
+		while True:
 
 			try:
 
@@ -168,26 +168,26 @@ class Socket:
 
 				data = client_socket.recv(self.CHUNK)
 
-				if get_request_method(data) == "GET": 
+				if get_request_method(data) == "GET":
 
-					file_name = get_file_name(data) 
-					
+					file_name = get_file_name(data)
+
 					#Empty file path or file does not exist - Error 404 Not Found
 					if not file_name or not os.path.exists(file_name):
 						http_response = generate_http_response(404, file_name)
 
 					#Wrong file type - 403
-					elif ends_with_(file_name): 
+					elif ends_with_(file_name):
 						http_response = generate_http_response(403, file_name)
 
-					else: 
+					else:
 						http_response = generate_http_response(200, file_name)
 
 					client_socket.send(http_response)
 
 					client_socket.close()
 
-			except (KeyboardInterrupt, SystemExit): 
+			except (KeyboardInterrupt, SystemExit):
 
 				self.sock.shutdown(socket.SHUT_RDWR)
 
--- http_server2.py
+++ http_server2.py
@@ -1,11 +1,13 @@
 import socket
-import sys 
-from urlparse import urlparse 
+import sys
+from urlparse import urlparse
 import os
 import time
 import signal
 import select
 import Queue
+import platform
+from datetime import datetime
 
 
 def signal_handler(sig, frame):
@@ -18,41 +20,39 @@ def get_last_modified_time(path):
 	if platform.system() == 'Windows':
 
 		return os.path.getctime(path)
-	
+
 	else:
-	
+
 		stat = os.stat(path)
-	
+
 		try:
-	
 			return stat.st_birthtime
-	
+
 		except AttributeError:
-	
+
 			return stat.st_mtime
 
 
 def generate_http_header(code, path):
-	
+
 	responses = {
 	200: '200 OK',
 	403: '403 Forbidden',
 	404: '404 Not Found'}
 
 	http_header = 'HTTP/1.1 ' + responses[code] + '\n'
-	
+
 	current_date = 'Date: ' + time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime()) + ' GMT\n'
 	http_header += current_date
 
-	'''
-	Last modified stuff
-	if code == 200: 
-		last_modified_time = 'Last Modified: ' + get_last_modified_time(path) + ' GMT' 
-	'''
+	if code == 200:
+		last_modified_time = 'Last Modified: ' + datetime.fromtimestamp(get_last_modified_time(path)).strftime("%a, %B %d, %Y %I:%M:%S") + ' GMT\n'
+		http_header += last_modified_time
+
 	server_name = 'Server: http_server1 (Simple)\n'
 	http_header += server_name
 
-	if code == 200: 
+	if code == 200:
 		http_header += 'Content-Type: text/html; charset=UTF-8\n'
 
 	CLRF = '\r\n\r\n'
@@ -60,8 +60,8 @@ def generate_http_header(code, path):
 	http_header+=connection_close
 
 	print(http_header)
-	
-	return http_header 
+
+	return http_header
 
 
 def generate_http_response(code, path):
@@ -72,16 +72,16 @@ def generate_http_response(code, path):
 		try:
 			file = open(path, 'r')
 			response_body = file.read()
-		except: 
+		except:
 			#If failure, 404 Not Found
 			generate_http_response(404, path)
-	
-	elif code == 403: 
-		
+
+	elif code == 403:
+
 		response_body = "<html><body><p>Error 403: Forbidden</p>"
-	
+
 	elif code == 404:
-		
+
 		response_body = "<html><body><p>Error 404: Not Found</p>"
 
 	response = response_header + response_body
@@ -110,16 +110,16 @@ def get_file_name(data):
 
 def ends_with_(name):
 
-	if name[-4:] == ".htm" or name[-4:] == "html": 
+	if name[-4:] == ".htm" or name[-4:] == "html":
 		return False
 
 
 def check_command_input():
 
-	if len(sys.argv) < 2: 
+	if len(sys.argv) < 2:
 		sys.stderr.write("ERROR no port provided")
 		sys.exit(1)
-	elif len(sys.argv) > 2: 
+	elif len(sys.argv) > 2:
 		sys.stderr.write("ERROR more than one porrt provided")
 		sys.exit(1)
 
@@ -128,7 +128,7 @@ class Socket:
 
 	CHUNK = 1024
 
-	def __init__(self): 
+	def __init__(self):
 
 		check_command_input()
 
@@ -142,7 +142,7 @@ class Socket:
 		self.create_socket()
 
 
-	def shutdown(self): 
+	def shutdown(self):
 
 		self.sock.shutdown(socket.SHUT_RDWR)
 
@@ -154,7 +154,7 @@ class Socket:
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 		self.sock.setblocking(0)
-		
+
 		self.sock.bind((self.host, self.port))
 
 		self.sock.listen(10)
@@ -164,71 +164,71 @@ class Socket:
 
 	def handle_request(self, data):
 
-		if get_request_method(data) == "GET": 
+		if get_request_method(data) == "GET":
 
-			file_name = get_file_name(data) 
-			
+			file_name = get_file_name(data)
+
 			#Empty file path or file does not exist - Error 404 Not Found
 			if not file_name or not os.path.exists(file_name):
 				http_response = generate_http_response(404, file_name)
 
 			#Wrong file type - 403
-			elif ends_with_(file_name): 
+			elif ends_with_(file_name):
 				http_response = generate_http_response(403, file_name)
 
-			else: 
+			else:
 				http_response = generate_http_response(200, file_name)
 
 		return http_response
 
-		
+
 	def run_forever(self):
 
 		inputs = [ self.sock ]
 		outputs = []
 		message_queues = {}
 
-		while inputs: 
+		while inputs:
 
 			try:
 
 				readable, writable, exceptional = select.select(inputs, outputs, inputs)
-				for s in readable: 
+				for s in readable:
 					if s is self.sock:
 						client_socket, client_address = s.accept()
 						client_socket.setblocking(0)
 						inputs.append(client_socket)
 						message_queues[client_socket] = Queue.Queue()
-					else: 
+					else:
 						data = s.recv(self.CHUNK)
-						if data: 
+						if data:
 							data = self.handle_request(data)
 							message_queues[s].put(data)
 							if s not in outputs:
 								outputs.append(s)
-						else: 
+						else:
 							if s in outputs:
 								outputs.remove(s)
 							inputs.remove(s)
 							s.close()
 							del message_queues[s]
-					
-					for s in writable: 
-						try: 
+
+					for s in writable:
+						try:
 							http_response = message_queues[s].get_nowait()
 						except Queue.Empty:
 							outputs.remove(s)
 						else:
-							s.send(http_response) 
+							s.send(http_response)
 
-					for s in exceptional: 
+					for s in exceptional:
 						inputs.remove(s)
 						if s in outputs:
 							outputs.remove(s)
 						s.close()
 						del message_queues[s]
-				
-			except (KeyboardInterrupt, SystemExit): 
+
+			except (KeyboardInterrupt, SystemExit):
 
 				self.sock.shutdown(socket.SHUT_RDWR)
 
